4d7b147754cd46b7e362f852d5eb920cece20d74
Kiet Mach <machkiet252003@gmail.com>
2024-04-14 15:49:05 +0700

    update database

---
+++ PHANHE1_PRJ/Database/2 InsertData/1 Insert_DONVI.sql
@@ -0,0 +1,8 @@
+--DU LIEU BANG DON VI
+INSERT INTO QL_TRUONGHOC_X.DONVI (MADV, TENDV, TRUONGDONVI)
+    SELECT 'VPK01','Van Phong Khoa', null from dual union all
+    SELECT 'HTTT1','Bo Mon He Thong Thong Tin', null from dual union all
+    SELECT 'CNPM1','Bo Mon Cong Nghe Phan Mem', null from dual union all
+    SELECT 'CNTT1','Bo Mon Cong Nghe Thong Tin', null from dual union all
+    SELECT 'TGMT1','Bo Mon Thi Giac May Tinh', null from dual union all
+    SELECT 'MTTVT','Bo Mon Mang May Tinh Va Vien Thong', null from dual ;
